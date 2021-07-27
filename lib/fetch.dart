b0d7507bf698d01e74660422a839587adadf45c1
kenkz447 <kenkz447@gmail.com>
2021-07-27 11:48:07 +0700

    feat: update Fetch basic usage test case

--- lib/fetch.dart
+++ lib/fetch.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/widgets.dart';
 
+class FetchException {
+  FetchException({required this.exception, required this.message});
+
+  final Exception exception;
+  final String message;
+}
+
+class FetchError {
+  FetchError({required this.httpStatus, required this.message});
+
+  final int httpStatus;
+  final String message;
+}
+
+class FetchState<TResponse> {
+  FetchState({this.loading, this.response, this.error, this.exception});
+
+  bool? loading;
+  TResponse? response;
+  FetchError? error;
+  FetchException? exception;
+}
+
 class Fetch<TResponse> extends StatefulWidget {
   Fetch({Key? key, required this.request, required this.builder})
       : super(key: key);
 
   final Function() request;
-  final Function(TResponse? response) builder;
+  final Function(FetchState<TResponse> fetchState) builder;
 
   @override
   _FetchState createState() => _FetchState<TResponse?>();
 }
 
 class _FetchState<TResponse> extends State<Fetch<TResponse>> {
-  TResponse? _response;
+  FetchState<TResponse> _fetchState = FetchState();
 
   @override
   void initState() {
@@ -21,14 +44,37 @@ class _FetchState<TResponse> extends State<Fetch<TResponse>> {
   }
 
   _request() async {
-    var response = await widget.request();
-    this.setState(() {
-      _response = response;
-    });
+    try {
+      setState(() {
+        _fetchState = FetchState(response: _fetchState.response, loading: true);
+      });
+
+      var response = await widget.request();
+
+      if (response is FetchError) {
+        this.setState(() {
+          _fetchState = FetchState<TResponse>(
+              loading: false, response: _fetchState.response, error: response);
+        });
+      } else {
+        this.setState(() {
+          _fetchState = FetchState<TResponse>(
+              loading: false, response: response, error: null);
+        });
+      }
+    } on Exception catch (exception) {
+      this.setState(() {
+        _fetchState = FetchState<TResponse>(
+            loading: false,
+            response: _fetchState.response,
+            exception: FetchException(
+                exception: exception, message: exception.toString()));
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return this.widget.builder(_response);
+    return this.widget.builder(_fetchState);
   }
 }
--- test/fetch_test.dart
+++ test/fetch_test.dart
@@ -6,39 +6,186 @@ import 'package:my_provider/index.dart';
 import 'package:http/http.dart' as http;
 
 import 'fetch_test.mocks.dart';
+import 'resources/product.dart';
 
+const jsonString = '{"id": 1, "name": "cat", "type": "pet"}';
+var productData = ProductFactory.create(jsonString);
+
+const fetchExceptionMessage = 'UNKNOW_EXCEPTION';
+const fetchErrorMessage = 'PRODUCT_NOT_EXIST';
 const fetchResponseText = 'RESPONSE';
+const nullText = 'NULL';
+const loadingText = 'NULL';
 
 MockClient? client;
 
-request() async {
-  final response = await client!.get(Uri.parse("https://domain.com/get-data"));
+requestString() async {
+  final response =
+      await client!.get(Uri.parse("https://domain.com/get-string"));
+  return response.body;
+}
+
+requestJson() async {
+  final response = await client!.get(Uri.parse("https://domain.com/get-json"));
+  var product = ProductFactory.create(response.body);
+  return product;
+}
+
+requestError() async {
+  final response = await client!.get(Uri.parse("https://domain.com/get-error"));
+  return FetchError(httpStatus: response.statusCode, message: response.body);
+}
+
+requestException() async {
+  final response =
+      await client!.get(Uri.parse("https://domain.com/get-exception"));
   return response.body;
 }
 
 @GenerateMocks([http.Client])
 main() {
-  testWidgets("Fetch: basic usage", (WidgetTester tester) async {
+  testWidgets("Fetch: request string", (WidgetTester tester) async {
     client = MockClient();
 
-    when(client!.get(Uri.parse("https://domain.com/get-data")))
+    when(client!.get(Uri.parse("https://domain.com/get-string")))
         .thenAnswer((_) async {
       await Future.delayed(Duration(seconds: 1));
       return http.Response(fetchResponseText, 200);
     });
 
     var fetch = Fetch<String?>(
-        request: request,
-        builder: (response) {
-          if (response == null) {
-            return Text("NULL", textDirection: TextDirection.ltr);
+        request: requestString,
+        builder: (fetchState) {
+          if (fetchState.response == null) {
+            return Text(nullText, textDirection: TextDirection.ltr);
           }
-          return Text(response, textDirection: TextDirection.ltr);
+          return Text(fetchState.response!, textDirection: TextDirection.ltr);
         });
 
     await tester.pumpWidget(fetch);
+    var valueFinder = find.text(nullText);
+    expect(valueFinder, findsOneWidget);
+
     await tester.pump(Duration(seconds: 1));
-    var valueFinder = find.text(fetchResponseText);
+    valueFinder = find.text(fetchResponseText);
+    expect(valueFinder, findsOneWidget);
+  });
+
+  testWidgets("Fetch: request json", (WidgetTester tester) async {
+    client = MockClient();
+
+    when(client!.get(Uri.parse("https://domain.com/get-json")))
+        .thenAnswer((_) async {
+      await Future.delayed(Duration(seconds: 1));
+      return http.Response('{"id": 1, "name": "cat", "type": "pet"}', 200);
+    });
+
+    var fetch = Fetch<Product?>(
+        request: requestJson,
+        builder: (fetchState) {
+          if (fetchState.response == null) {
+            return Text(nullText, textDirection: TextDirection.ltr);
+          }
+
+          return Text(fetchState.response!.name!,
+              textDirection: TextDirection.ltr);
+        });
+
+    await tester.pumpWidget(fetch);
+    var valueFinder = find.text(nullText);
+    expect(valueFinder, findsOneWidget);
+
+    await tester.pump(Duration(seconds: 1));
+    valueFinder = find.text(productData.name!);
+    expect(valueFinder, findsOneWidget);
+  });
+
+  testWidgets("Fetch: server status error", (WidgetTester tester) async {
+    client = MockClient();
+
+    when(client!.get(Uri.parse("https://domain.com/get-error")))
+        .thenAnswer((_) async {
+      await Future.delayed(Duration(seconds: 1));
+      return http.Response(fetchErrorMessage, 404);
+    });
+
+    var fetch = Fetch<Product?>(
+        request: requestError,
+        builder: (response) {
+          if (response.loading == true) {
+            return Text(nullText, textDirection: TextDirection.ltr);
+          }
+
+          return Text(response.error!.message,
+              textDirection: TextDirection.ltr);
+        });
+
+    await tester.pumpWidget(fetch);
+    var valueFinder = find.text(nullText);
+    expect(valueFinder, findsOneWidget);
+
+    await tester.pump(Duration(seconds: 1));
+    valueFinder = find.text(fetchErrorMessage);
+    expect(valueFinder, findsOneWidget);
+  });
+
+  testWidgets("Fetch: server status error", (WidgetTester tester) async {
+    client = MockClient();
+
+    when(client!.get(Uri.parse("https://domain.com/get-exception")))
+        .thenAnswer((_) async {
+      throw Exception(fetchExceptionMessage);
+    });
+
+    var fetch = Fetch<Product?>(
+        request: requestException,
+        builder: (response) {
+          if (response.exception != null) {
+            return Text(response.exception!.message,
+                textDirection: TextDirection.ltr);
+          }
+
+          if (response.response == null) {
+            return Text(nullText, textDirection: TextDirection.ltr);
+          }
+
+          return Text("", textDirection: TextDirection.ltr);
+        });
+
+    await tester.pumpWidget(fetch);
+    var valueFinder = find.text(nullText);
+    expect(valueFinder, findsOneWidget);
+
+    await tester.pump(Duration(seconds: 1));
+    valueFinder = find.text("Exception: $fetchExceptionMessage");
+    expect(valueFinder, findsOneWidget);
+  });
+
+  testWidgets("Fetch: loading", (WidgetTester tester) async {
+    client = MockClient();
+
+    when(client!.get(Uri.parse("https://domain.com/get-string")))
+        .thenAnswer((_) async {
+      await Future.delayed(Duration(seconds: 1));
+      return http.Response(fetchResponseText, 200);
+    });
+
+    var fetch = Fetch<String?>(
+        request: requestString,
+        builder: (fetchState) {
+          if (fetchState.loading == true) {
+            return Text(loadingText, textDirection: TextDirection.ltr);
+          }
+
+          return Text(fetchState.response!, textDirection: TextDirection.ltr);
+        });
+
+    await tester.pumpWidget(fetch);
+    var valueFinder = find.text(loadingText);
+    expect(valueFinder, findsOneWidget);
+
+    await tester.pump(Duration(seconds: 1));
+    valueFinder = find.text(fetchResponseText);
     expect(valueFinder, findsOneWidget);
   });
 }
