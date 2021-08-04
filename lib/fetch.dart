5023499fed2438651322c84d6835916a134467f4
quangkhang20899 <quangkhang20899@gmail.com>
2021-08-04 11:42:46 +0700

    feat: implement FetchCache

--- lib/fetch.dart
+++ lib/fetch.dart
@@ -31,27 +31,50 @@ class FetchState<TResponse, TParams> {
   FetchException? exception;
 }
 
+class FetchCallback {
+  FetchCallback(this.providerKey, this.callbackFunc);
+
+  final String providerKey;
+  final Function(FetchState, bool) callbackFunc;
+}
+
 class Fetch<TResponse, TParams> extends StatefulWidget {
   static Map<String, dynamic> mapFetch = Map();
-  static bool isCaching = false;
+  static Map<String, bool> mapIsCaching = Map();
+  static List<FetchCallback> callBacksUpdateFetchState = [];
+
+  static registerCallback(FetchCallback fetchCallback) {
+    Fetch.callBacksUpdateFetchState.add(fetchCallback);
+  }
+
+  static setFetchState(String providerKey, dynamic fetchState,
+      {bool hasOnSuccess = false}) {
+    mapFetch[providerKey] = fetchState;
+    var callbacks = callBacksUpdateFetchState
+        .where((element) => element.providerKey == providerKey);
+
+    for (var callback in callbacks) {
+      callback.callbackFunc(fetchState, hasOnSuccess);
+    }
+  }
 
   static setResponse(String providerKey, dynamic response) {
     Provider(providerKey: providerKey, value: response).setValue(response);
   }
 
-  Fetch(
-      {Key? key,
-      required this.request,
-      required this.builder,
-      this.params,
-      this.lazy = false,
-      this.onSuccess,
-      this.onError,
-      this.providerKey,
-      this.cacheFirst,
-      this.cacheDuration,
-      this.onInit})
-      : super(key: key);
+  Fetch({
+    Key? key,
+    required this.request,
+    required this.builder,
+    this.params,
+    this.lazy = false,
+    this.onSuccess,
+    this.onError,
+    this.providerKey,
+    this.cacheFirst,
+    this.cacheDuration,
+    this.onInit,
+  }) : super(key: key);
 
   final String? providerKey;
   final bool lazy;
@@ -82,29 +105,43 @@ class _FetchState<TResponse, TParams> extends State<Fetch<TResponse, TParams>> {
 
   @override
   void initState() {
-    super.initState();
-
     if (widget.providerKey != null) {
-      Provider.registerCallback(
-          ProviderCallback(widget.providerKey!, this._onUpdateCallback));
+      Provider.registerCallback(ProviderCallback(
+          widget.providerKey!, this._onUpdateResponseCallback));
+      Fetch.registerCallback(FetchCallback(
+        widget.providerKey!,
+        this._onUpdateFetchStateCallback,
+      ));
     }
-    if (Fetch.mapFetch.keys
-        .where((element) => element == widget.providerKey)
-        .isEmpty)
+    if (widget.cacheFirst != true ||
+        Fetch.mapFetch.keys
+            .where((element) => element == widget.providerKey!)
+            .isEmpty) {
       _fetchState = FetchState(fetch: _request);
-    else {
-      if (widget.onInit != null) {
-        widget.onInit!(Fetch.mapFetch[widget.providerKey!]);
-      }
+    } else {
       _fetchState = Fetch.mapFetch[widget.providerKey!];
+      if (widget.onInit != null) {
+        widget.onInit!(_fetchState);
+      }
     }
     if (!widget.lazy) {
-      if (Fetch.mapFetch.keys
-              .where((element) => element == widget.providerKey)
-              .isEmpty ||
-          (widget.cacheDuration == null && widget.cacheFirst == null))
+      if(widget.providerKey==null)
+        {
+          _request(null);
+        }
+     else if (Fetch.mapIsCaching[widget.providerKey!] == false ||
+          Fetch.mapFetch.keys
+              .where((element) => element == widget.providerKey!)
+              .isEmpty) {
         _request(null);
+      }
     }
+    if(Fetch.mapFetch.keys
+        .where((element) => element == widget.providerKey!)
+        .isEmpty && widget.providerKey!=null)
+      Fetch.mapFetch[widget.providerKey!]=_fetchState;
+
+    super.initState();
   }
 
   @override
@@ -114,30 +151,58 @@ class _FetchState<TResponse, TParams> extends State<Fetch<TResponse, TParams>> {
     super.dispose();
   }
 
-  _onUpdateCallback(value) {
+  _onUpdateResponseCallback(value) {
     setState(() {
       _fetchState = FetchState(
           fetch: _request, loading: false, response: value, error: null);
     });
   }
 
+  _onUpdateFetchStateCallback(FetchState fetchState, hasOnSuccess) {
+    if (this._disposed) {
+      return;
+    }
+    setState(() {
+      _fetchState = Fetch.mapFetch[widget.providerKey!];
+//      _fetchState.response = fetchState.response;
+//      _fetchState.error = fetchState.error;
+//      _fetchState.loading = fetchState.loading;
+//      _fetchState.exception = fetchState.exception;
+    });
+    if (hasOnSuccess == true) {
+      if (widget.onSuccess != null) {
+        widget.onSuccess!(_fetchState.response, _fetchState);
+      }
+    }
+  }
+
   _request(TParams? params) async {
-    if (Fetch.isCaching != true)
+    if (Fetch.mapIsCaching[widget.providerKey] != true)
       try {
         if (!(widget.cacheDuration == null && widget.cacheFirst == null)) {
-          Fetch.isCaching = true;
+          Fetch.mapIsCaching[widget.providerKey!] = true;
           _timer = Timer.periodic(Duration(seconds: 1), (timer) {
             if (countCacheDuration == widget.cacheDuration) {
-              Fetch.isCaching = false;
+              countCacheDuration = 0;
+              Fetch.mapIsCaching[widget.providerKey!] = false;
               timer.cancel();
             }
             countCacheDuration += 1000;
           });
         }
-        setState(() {
+        if (widget.providerKey == null)
+          setState(() {
+            _fetchState = FetchState(
+                fetch: _request, response: _fetchState.response, loading: true);
+          });
+        else {
           _fetchState = FetchState(
               fetch: _request, response: _fetchState.response, loading: true);
-        });
+          Fetch.setFetchState(
+            widget.providerKey!,
+            _fetchState,
+          );
+        }
 
         var response = params != null
             ? await widget.request(params)
@@ -147,21 +212,32 @@ class _FetchState<TResponse, TParams> extends State<Fetch<TResponse, TParams>> {
           return;
         }
 
-        this.setState(() {
+        if (widget.providerKey == null) {
+          this.setState(() {
+            _fetchState = FetchState(
+                fetch: _request,
+                loading: false,
+                response: response,
+                error: null);
+          });
+        } else {
           _fetchState = FetchState(
               fetch: _request, loading: false, response: response, error: null);
-        });
-
-        if (Fetch.mapFetch.keys
-                .where((element) => element == widget.providerKey)
-                .isEmpty &&
-            widget.providerKey != null)
-          Fetch.mapFetch[widget.providerKey!] = _fetchState;
-
-        if (widget.onSuccess != null) {
-          widget.onSuccess!(response, Fetch.mapFetch[widget.providerKey!]);
+          Fetch.setFetchState(
+            widget.providerKey!,
+            _fetchState,
+            hasOnSuccess: true,
+          );
         }
-        Fetch.isCaching = false;
+
+        if (widget.providerKey == null) if (widget.onSuccess != null) {
+          widget.onSuccess!(response, _fetchState);
+        }
+//        if (widget.providerKey != null)
+//          Fetch.mapIsCaching[widget.providerKey!] = false;
+        if (_timer != null && _timer!.isActive) {
+          _timer!.cancel();
+        }
       } on FetchError catch (error) {
         if (this._disposed) {
           return;
--- test/fetch_cache_test.dart
+++ test/fetch_cache_test.dart
@@ -40,8 +40,8 @@ main() {
             builder: getProductBuidler),
         Fetch<Product?, ProductFetchParams>(
             providerKey: "FETCH_PRODUCT",
-            onSuccess: (response, fetchState) {
-              fetchState_2 = fetchState;
+            onSuccess: (response, fetchState2) {
+              fetchState_2 = fetchState2;
             },
             params: params,
             request: getProduct,
@@ -75,54 +75,138 @@ main() {
     var requestCount = 0;
     var successCount = 0;
 
-    var fetch = Column(
-      children: [
-        Fetch<Product?, ProductFetchParams>(
-            providerKey: "FETCH_PRODUCT",
-            cacheDuration: 10000,
-            onSuccess: (response, fetchState) {
-              successCount++;
-              fetchState_1 = fetchState;
-            },
-            params: params,
-            request: (params) async {
-              requestCount++;
-              return await getProduct(params);
-            },
-            builder: (fetchState) {
-              if (fetchState.loading == true) {
-                return Text(
-                  loadingText,
-                  textDirection: TextDirection.rtl,
-                );
-              }
+    var fetch = Fetch<Product?, ProductFetchParams>(
+        providerKey: "FETCH_PRODUCT",
+        cacheDuration: 10000,
+        onSuccess: (response, fetchState) {
+          successCount++;
+          fetchState_1 = fetchState;
+        },
+        params: params,
+        request: (params) async {
+          requestCount++;
+          return await getProduct(params);
+        },
+        builder: (fetchState) {
+          if (fetchState.loading == true) {
+            return Text(
+              loadingText,
+              textDirection: TextDirection.rtl,
+            );
+          }
 
-              return Fetch<Product?, ProductFetchParams>(
-                  providerKey: "FETCH_PRODUCT",
-                  cacheFirst: true,
-                  onInit: (fetchStateInit) {
-                    fetchState_2 = fetchStateInit;
-                  },
-                  onSuccess: (response, fetchState) {
-                    successCount++;
-                    fetchState_2 = fetchState;
-                  },
-                  params: params,
-                  request: (params) async {
-                    requestCount++;
-                    return await getProduct(params);
-                  },
-                  builder: getProductBuidler);
-            }),
-      ],
-    );
-
+          if (fetchState.response != null)
+            return Fetch<Product?, ProductFetchParams>(
+                providerKey: "FETCH_PRODUCT",
+                cacheFirst: true,
+                onInit: (fetchStateInit) {
+                  fetchState_2 = fetchStateInit;
+                },
+                onSuccess: (response, fetchState) {
+                  successCount++;
+                  fetchState_2 = fetchState;
+                },
+                params: params,
+                request: (params) async {
+                  requestCount++;
+                  return await getProduct(params);
+                },
+                builder: getProductBuidler);
+          return Text(
+            "NULL",
+            textDirection: TextDirection.rtl,
+          );
+        });
     await tester.pumpWidget(fetch);
     await tester.pump(Duration(seconds: 2));
+
     expect(requestCount, 1);
     expect(successCount, 1);
-
     expect(fetchState_1, fetchState_2);
-    await tester.pump(Duration(seconds: 10));
+  });
+
+  testWidgets("Fetch caching: outdated cache", (WidgetTester tester) async {
+    var client = MockClient();
+    setClient(client);
+
+    when(client
+            .get(Uri.parse("https://domain.com/products/${params.productId}")))
+        .thenAnswer((_) async {
+      await Future.delayed(Duration(seconds: 1));
+      return http.Response(productJsonString, 200);
+    });
+
+    dynamic fetchState_1;
+    dynamic fetchState_2;
+
+    var requestCount = 0;
+    var successCount = 0;
+
+    var fetch = Fetch<Product?, ProductFetchParams>(
+        providerKey: "FETCH_PRODUCT",
+        cacheDuration: 10000,
+        onSuccess: (response, fetchState) {
+          successCount++;
+          fetchState_1 = fetchState;
+        },
+        params: params,
+        request: (params) async {
+          requestCount++;
+          return await getProduct(params);
+        },
+        builder: (fetchState) {
+          if (fetchState.loading == true) {
+            return Text(
+              loadingText,
+              textDirection: TextDirection.rtl,
+            );
+          }
+
+          if (fetchState.response != null)
+            return Fetch<Product?, ProductFetchParams>(
+                providerKey: "FETCH_PRODUCT",
+                cacheFirst: true,
+                onInit: (fetchStateInit) {
+                  fetchState_2 = fetchStateInit;
+                },
+                onSuccess: (response, fetchState) {
+                  successCount++;
+                  fetchState_2 = fetchState;
+                },
+                params: params,
+                request: (params) async {
+                  requestCount++;
+                  return await getProduct(params);
+                },
+                builder: getProductBuidler);
+          return Text(
+            "NULL",
+            textDirection: TextDirection.rtl,
+          );
+        });
+    await tester.pumpWidget(fetch);
+    await tester.pump(Duration(seconds: 2));
+
+    await tester.pump(Duration(seconds: 12));
+    Fetch.mapIsCaching["FETCH_PRODUCT"] = false;
+    var fetch2 = Fetch<Product?, ProductFetchParams>(
+        key: GlobalKey(),
+        providerKey: "FETCH_PRODUCT",
+        cacheFirst: true,
+        onInit: (fetchStateInit) {},
+        onSuccess: (response, fetchState) async {
+          successCount++;
+        },
+        params: params,
+        request: (params) async {
+          requestCount++;
+          return await getProduct(params);
+        },
+        builder: getProductBuidler);
+
+    await tester.pumpWidget(fetch2);
+    await tester.pump(Duration(seconds: 2));
+    expect(requestCount, 2);
+    expect(successCount, 2);
   });
 }
