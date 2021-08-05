d16a7288d329ed40eacabf7dfea2aac0d6da974f
nguyenphuochai1998 <haito1998@gmail.com>
2021-08-05 11:40:16 +0700

    update_dispose_provider

--- lib/provider.dart
+++ lib/provider.dart
@@ -45,6 +45,12 @@ class Provider<TValue> {
   }
 
   dispose() {
-    throw UnimplementedError();
+    var callbacks = Provider.callbacks
+        .where((element) => element.providerKey == this.providerKey);
+
+    for (var callback in callbacks) {
+      callback.callbackFunc(null);
+    }
+    Provider.providers.removeWhere((key, value) => key == providerKey);
   }
 }
